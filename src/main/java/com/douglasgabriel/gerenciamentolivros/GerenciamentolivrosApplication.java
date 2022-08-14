09647d768e13a484110e8f56fc9b41b49b3ef8d0
Douglas Gabriel <douglaasg11@gmail.com>
2022-08-14 14:27:30 -0300

    Projeto Criado

---
+++ src/main/java/com/douglasgabriel/gerenciamentolivros/GerenciamentolivrosApplication.java
@@ -0,0 +1,13 @@
+package com.douglasgabriel.gerenciamentolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentolivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentolivrosApplication.class, args);
+	}
+
+}
