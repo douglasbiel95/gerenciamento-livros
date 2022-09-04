c80f3cef1f9a1a8857781b981f53917413ce7c65
Douglas Gabriel <douglaasg11@gmail.com>
2022-09-04 13:51:44 -0300

    Criando Repositorios para as Entidades

---
+++ src/main/java/com/douglasgabriel/gerenciamentolivros/livros/LivrosRepository.java
@@ -0,0 +1,7 @@
+package com.douglasgabriel.gerenciamentolivros.livros;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivrosRepository extends JpaRepository<Livros, Long> {
+
+}
